2ae00c8c02950a26d3615aee4e5eff9f1f919e02
Rüdiger Klaehn <rklaehn@gmail.com>
2014-12-27 23:49:07 +0100

    Moved CountingOrder into test namespace

---
+++ src/test/scala/com/rklaehn/interval/CountingOrder.scala
@@ -0,0 +1,16 @@
+package com.rklaehn.interval
+
+import spire.algebra.Order
+
+/**
+ * A wrapper for the spire order typeclass that counts invocations of compare
+ */
+class CountingOrder[T:Order] extends Order[T] {
+  val wrapped = implicitly[Order[T]]
+  var count = 0
+
+  override def compare(x: T, y: T): Int = {
+    count += 1
+    wrapped.compare(x, y)
+  }
+}
