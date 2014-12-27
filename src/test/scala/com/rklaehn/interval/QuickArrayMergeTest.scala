173e2f31bbd4039263a4ca246dcbc0f70f5c16a8
Rüdiger Klaehn <rklaehn@gmail.com>
2014-12-27 23:48:22 +0100

    Added manual test for QuickArrayMerge that attempts to find a worst case

---
+++ src/test/scala/com/rklaehn/interval/QuickArrayMergeTest.scala
@@ -0,0 +1,22 @@
+package com.rklaehn.interval
+
+import spire.implicits._
+import org.junit.Assert._
+import org.junit.Test
+
+import scala.reflect.ClassTag
+
+
+class QuickArrayMergeTest {
+
+  @Test
+  def testWorstCase(): Unit = {
+    val a = Array.range(0,100).map(_ * 2)
+    val b = Array.range(1,100).map(_ * 2)
+    val o = new CountingOrder[Int]
+    val r = QuickArrayMerge.merge(a,b)(o, ClassTag.Int)
+    println(r.sorted.corresponds(r)(_ == _))
+    println(r.size)
+    println(o.count)
+  }
+}
