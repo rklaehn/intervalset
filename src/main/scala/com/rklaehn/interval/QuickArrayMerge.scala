574c6201f30e0b0216abf1c4c69bc9918680804b
Rüdiger Klaehn <rklaehn@gmail.com>
2014-12-27 23:47:20 +0100

    Reworked QuickArrayMerge to not use a builder
    The result size is always m+n, so a builder is useless and wasteful

--- src/main/scala/com/rklaehn/interval/QuickArrayMerge.scala
+++ src/main/scala/com/rklaehn/interval/QuickArrayMerge.scala
@@ -4,25 +4,6 @@ import spire.algebra.Order
 
 import scala.reflect.ClassTag
 
-object CountingOrder {
-  def apply[T:Order, U](f: Order[T] => U) = {
-    val countingOrder = new CountingOrder[T]
-    val result = f(countingOrder)
-    println("Order was called " + countingOrder.count + " times")
-    result
-  }
-}
-
-class CountingOrder[T:Order] extends Order[T] {
-  val wrapped = implicitly[Order[T]]
-  var count = 0
-
-  override def compare(x: T, y: T): Int = {
-    count += 1
-    wrapped.compare(x, y)
-  }
-}
-
 object QuickArrayMerge {
 
   def binarySearch[T: Order](array: Array[T], key: T, from: Int, until: Int): Int = {
@@ -46,17 +27,16 @@ object QuickArrayMerge {
     -(low + 1)
   }
 
-  def merge[T: Order : ClassTag](a: Array[T], b: Array[T]): Array[T] = {
-    val builder = Array.newBuilder[T]
-    builder.sizeHint(a.length + b.length)
+  def merge[T: Order : ClassTag](a: Array[T], b: Array[T]): Array[T] = /* if(a.length > b.length) merge(b, a) else */ {
+    val r = new Array[T](a.length + b.length)
 
-    def merge0(a0: Int, a1: Int, b0: Int, b1: Int): Unit = {
+    def merge0(a0: Int, a1: Int, b0: Int, b1: Int, i: Int): Int = {
       if (a0 == a1) {
-        for (bi <- b0 until b1)
-          builder += b(bi)
+        System.arraycopy(b, b0, r, i, b1 - b0)
+        i + b1 - b0
       } else if (b0 == b1) {
-        for (ai <- a0 until a1)
-          builder += a(ai)
+        System.arraycopy(a, a0, r, i, a1 - a0)
+        i + a1 - a0
       } else {
         val am = (a0 + a1) / 2
         val res = binarySearch(b, a(am), b0, b1)
@@ -64,25 +44,25 @@ object QuickArrayMerge {
           // same elements
           val bm = res
           // merge everything below a(am) with everything below the found element
-          merge0(a0, am, b0, bm)
+          val i1 = merge0(a0, am, b0, bm, i)
           // add the elements a(am) and b(bm)
-          builder += a(am)
-          builder += b(bm)
+          r(i1) = a(am)
+          r(i1 + 1) = b(bm)
           // merge everything above a(am) with everything above the found element
-          merge0(am + 1, a1, bm + 1, b1)
+          merge0(am + 1, a1, bm + 1, b1, i1 + 2)
         } else {
           val bm = -res - 1
           // merge everything below a(am) with everything below the found insertion point
-          merge0(a0, am, b0, bm)
+          val i1 = merge0(a0, am, b0, bm, i)
           // add a(am)
-          builder += a(am)
+          r(i1) = a(am)
           // everything above a(am) with everything above the found insertion point
-          merge0(am + 1, a1, bm, b1)
+          merge0(am + 1, a1, bm, b1, i1 + 1)
         }
       }
     }
 
-    merge0(0, a.length, 0, b.length)
-    builder.result()
+    merge0(0, a.length, 0, b.length, 0)
+    r
   }
 }
