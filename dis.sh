5f096d7dcb4b94637187fd68a6eba118efdbe54d
Rüdiger Klaehn <rklaehn@gmail.com>
2017-09-11 22:11:38 +0200

    Fiddling with printCommand

--- dis.sh
+++ dis.sh
@@ -5,6 +5,5 @@ export CLASSPATH=$PWD/.jvm/target/scala-2.12/classes/:$CLASSPATH
 export CLASSPATH=$PWD/.jvm/target/scala-2.12/test-classes/:$CLASSPATH
 export CLASSPATH=$HOME/.ivy2/cache/org.scala-lang/scala-reflect/jars/scala-reflect-2.12.1.jar:$CLASSPATH
 export CLASSPATH=$HOME/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.12.1.jar:$CLASSPATH
-echo $CLASSPATH
 sbt intervalsetJVM/test:compile
-java -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand=print,*DisassemblyTestApp.* com.rklaehn.interval.DisassemblyTestApp
+java -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand='print,com.rklaehn.interval.DisassemblyTestApp' com.rklaehn.interval.DisassemblyTestApp
