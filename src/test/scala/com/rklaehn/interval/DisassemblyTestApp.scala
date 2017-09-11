97e3fba5270fd00d52b0ae28b30bc2dfa5afb277
RÃ¼diger Klaehn <rklaehn@gmail.com>
2017-09-11 22:18:05 +0200

    More invocations, less @inline

--- src/test/scala/com/rklaehn/interval/DisassemblyTestApp.scala
+++ src/test/scala/com/rklaehn/interval/DisassemblyTestApp.scala
@@ -4,29 +4,34 @@ import java.lang.Long.numberOfLeadingZeros
 
 object DisassemblyTestApp {
 
-  @inline final def toPrefix(key: Long): Long = key - Long.MinValue
+  final def toPrefix(key: Long): Long = key - Long.MinValue
 
-  @inline final def fromPrefix(key: Long): Long = key + Long.MinValue
+  final def fromPrefix(key: Long): Long = key + Long.MinValue
 
-  @inline final def unsigned_<(i: Long, j: Long) = (i < j) ^ (i < 0L) ^ (j < 0L)
+  final def unsigned_<(i: Long, j: Long) = (i < j) ^ (i < 0L) ^ (j < 0L)
 
-  @inline final def levelAbove(a: Long, b: Long): Byte =
+  final def levelAbove(a: Long, b: Long): Byte =
     (63 - numberOfLeadingZeros(a ^ b)).toByte
 
-  @inline final def maskAbove(prefix: Long, bit: Byte) = {
+  final def maskAbove(prefix: Long, bit: Byte) = {
     // this is not the same as (-1L << (bit + 1)) due to the somewhat strange behavior of the java shift operator
     // -1L << 64 gives -1L, whereas (-1L << 63) << 1 gives 0L like we need
     prefix & ((-1L << bit) << 1)
   }
 
-  @inline final def zeroAt(value: Long, bit: Byte) =
+  final def zeroAt(value: Long, bit: Byte) =
     (value & (1L << bit)) == 0L
 
-  @inline final def hasMatchAt(key: Long, prefix: Long, level: Byte) =
+  final def hasMatchAt(key: Long, prefix: Long, level: Byte) =
     maskAbove(key, level) == prefix
 
   def main(args: Array[String]): Unit = {
-    val x = levelAbove(100L, 10L)
-    println(x)
+    var i = 0
+    var sum = 0L
+    while(i < 100000) {
+      sum += levelAbove(i, i * 3 + 2)
+      i += 1
+    }
+    println(sum)
   }
 }
