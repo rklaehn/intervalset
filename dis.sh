454f2718e77fa9c7f7138fcc82c80eb13e54ab36
RÃ¼diger Klaehn <rklaehn@gmail.com>
2017-09-11 22:01:24 +0200

    Add some code to disassemble

--- dis.sh
+++ dis.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 export PWD=`pwd`
-echo $PWD
 export CLASSPATH=''
-export CLASSPATH=/Users/rklaehn/projects_git/intervalset/.jvm/target/scala-2.12/classes/:$CLASSPATH
-export CLASSPATH=/Users/rklaehn/projects_git/intervalset/.jvm/target/scala-2.12/test-classes/:$CLASSPATH
-export CLASSPATH=/Users/rklaehn/.ivy2/cache/org.scala-lang/scala-reflect/jars/scala-reflect-2.12.1.jar:$CLASSPATH
-export CLASSPATH=/Users/rklaehn/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.12.1.jar:$CLASSPATH
+export CLASSPATH=$PWD/.jvm/target/scala-2.12/classes/:$CLASSPATH
+export CLASSPATH=$PWD/.jvm/target/scala-2.12/test-classes/:$CLASSPATH
+export CLASSPATH=$HOME/.ivy2/cache/org.scala-lang/scala-reflect/jars/scala-reflect-2.12.1.jar:$CLASSPATH
+export CLASSPATH=$HOME/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.12.1.jar:$CLASSPATH
 echo $CLASSPATH
 sbt intervalsetJVM/test:compile
 java -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly com.rklaehn.interval.DisassemblyTestApp
